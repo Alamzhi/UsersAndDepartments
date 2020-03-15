5f9f239976284d8db003ae70fda0ed486c42d938
Sayan Damdinov <damdinov@gmail.com>
2020-03-16 00:13:36 +0300

    конфигурация контекста и бд

--- UsersAndDepartments/Contexts/UserContext.cs
+++ UsersAndDepartments/Contexts/UserContext.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using UsersAndDepartments.Entities;
 
 namespace UsersAndDepartments.Contexts
 {
 	public sealed class UserContext : DbContext
 	{
+		public UserContext() { }
+
 		public UserContext(DbContextOptions<UserContext> options)
 			: base(options)
-		{
-			Database.EnsureCreated();
-		}
+		{ }
 
 		public DbSet<User> Users { get; set; }
+
 		public DbSet<Department> Departments { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				IConfigurationRoot configuration = new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json")
+					.Build();
+				optionsBuilder.UseSqlServer(configuration.GetConnectionString("DbConnection"));
+			}
+		}
 	}
 }
