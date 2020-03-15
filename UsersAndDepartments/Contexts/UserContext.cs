ab0df4be5251444246d44ce5449bc9cf8ac20e24
Sayan Damdinov <damdinov@gmail.com>
2020-03-15 23:16:17 +0300

    Подключен EF Core

---
+++ UsersAndDepartments/Contexts/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UsersAndDepartments.Entities;
+
+namespace UsersAndDepartments.Contexts
+{
+	public sealed class UserContext : DbContext
+	{
+		public UserContext(DbContextOptions<UserContext> options)
+			: base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Department> Departments { get; set; }
+	}
+}
--- UsersAndDepartments/Startup.cs
+++ UsersAndDepartments/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using UsersAndDepartments.Contexts;
 
 namespace UsersAndDepartments
 {
@@ -26,6 +28,10 @@ namespace UsersAndDepartments
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+			//подключение к БД
+			services.AddDbContext<UserContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +52,8 @@ namespace UsersAndDepartments
 			{
 				endpoints.MapControllers();
 			});
+
+
 		}
 	}
 }
